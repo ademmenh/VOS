1e078137020c2f6e4bb44c71e9ccbcb89f335d19
ademmenh <ademmenh777@gmail.com>
2026-02-19 03:29:21 +0100

    feat: added kernel stack, modified paging, and memory allocaiton

--- vos/include/memory/pmm.h
+++ vos/include/memory/pmm.h
@@ -2,14 +2,19 @@
 #define PMM_H
 
 #include <stdint.h>
+#include "memory/vmm.h"
 
+extern uint32_t KERNEL_START;
 extern uint32_t KERNEL_END;
 
 #define PAGE_SIZE      4096
 #define PDE_COUNT      1024
 #define PTE_COUNT      1024
 #define MAX_FRAMES 1048576   // 4GB / 4KB
-#define KERNEL_FRAMES (KERNEL_END / PAGE_SIZE + 1)
+
+#define KERNEL_CODE_FRAMES  (((uint32_t)&KERNEL_END - (uint32_t)KERNEL_OFFSET + PAGE_SIZE - 1) / PAGE_SIZE)
+#define KERNEL_STACK_FRAMES 4
+#define KERNEL_FRAMES      (KERNEL_CODE_FRAMES + KERNEL_STACK_FRAMES)
 
 void initPmm(uint32_t total_frames);
 int  allocPhysicalPage(void);
--- vos/kernel/memory/gdt.asm
+++ vos/kernel/memory/gdt.asm
@@ -3,7 +3,6 @@ global loadGDT
 loadGDT:
     mov eax, [esp+4]
     lgdt [eax]
-    ; far jump to flush pipeline and load CS
     jmp 0x08:protected_mode
 
 protected_mode:
